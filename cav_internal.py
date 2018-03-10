2d9212cd234c0cc892c54bb691a976292bc9c706
WRHX <wouterhaaxman@gmail.com>
2018-03-10 19:02:12 +0100

    Initial commit

---
+++ cav_internal.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class FabryPerotCavity(object):
+    self.__init__():
+        self.test = 'test'
+
+
+    def printable(self):
+        print(self.test)
+
+if __name__ == '__main__':
+    FPcav = FabryPerotCavity()
+    FPcav.printable()
